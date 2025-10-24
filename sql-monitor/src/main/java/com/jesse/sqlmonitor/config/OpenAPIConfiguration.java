a814f741ee56ad0d622cb7a4af26608754d7338e
浩进 朱 <3191955858@qq.com>
2025-10-24 16:07:42 +0800

    Swagger 配置类。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/config/OpenAPIConfiguration.java
@@ -0,0 +1,33 @@
+package com.jesse.sqlmonitor.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/** Swagger 配置类。*/
+@Configuration
+public class OpenAPIConfiguration
+{
+    @Bean
+    public OpenAPI openAPI()
+    {
+        return new
+        OpenAPI().info(
+            new Info().title("MySQL 数据库指标监视器")
+                .version("1.0.0")
+                .description("实时监控 MySQL 的各项指标。")
+        );
+    }
+
+    @Bean
+    public GroupedOpenApi publicAPI()
+    {
+        return
+        GroupedOpenApi.builder()
+            .group("sqlmonitor")
+            .pathsToMatch("/api/sql-monitor/**")
+            .build();
+    }
+}
