a7e898e126265d6c3e2d10392ed687bbce48e004
浩进 朱 <3191955858@qq.com>
2025-10-24 16:02:31 +0800

    数据库指标类型枚举。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/indicator_record/entity/IndicatorType.java
@@ -0,0 +1,11 @@
+package com.jesse.sqlmonitor.indicator_record.entity;
+
+/** 数据库指标类型枚举。*/
+public enum IndicatorType
+{
+    ConnectionUsage,
+    DatabaseSize,
+    InnodbBufferCacheHitRate,
+    NetWorkTraffic,
+    QPSResult
+}
