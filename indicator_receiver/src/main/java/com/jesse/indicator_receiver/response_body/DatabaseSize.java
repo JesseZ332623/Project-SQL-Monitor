722d40f38fd9b3008539bd905e75b7a17098fc3c
浩进 朱 <3191955858@qq.com>
2025-10-24 15:45:11 +0800

    数据库大小数据响应。

---
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/response_body/DatabaseSize.java
@@ -0,0 +1,33 @@
+package com.jesse.indicator_receiver.response_body;
+
+import com.jesse.indicator_receiver.response_body.base.ResponseBase;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/** 数据库大小数据响应。*/
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Schema(
+    description = "数据库大小数据",
+    requiredProperties = {
+        "sizeBytes", "sizeMBytes"
+    }
+)
+public class DatabaseSize extends ResponseBase<DatabaseSize>
+{
+    @Schema(description = "字节数")
+    private long sizeBytes;
+
+    @Schema(description = "兆字节数")
+    private double sizeMBytes;
+
+    @Schema(description = "该数据库下所有表的大小（单位：MB）")
+    private Map<String, Double> tableSizes = new LinkedHashMap<>();
+}
