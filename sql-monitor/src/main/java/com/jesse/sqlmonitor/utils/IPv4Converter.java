1a49a946ccea9eafc06effa2f73e9c63cbaec58b
浩进 朱 <3191955858@qq.com>
2025-10-24 16:05:53 +0800

    本机 IP 地址获取器（仅限 Windows）。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/utils/IPv4Converter.java
@@ -0,0 +1,57 @@
+package com.jesse.sqlmonitor.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import static com.jesse.sqlmonitor.utils.SystemIPv4Getter.getLocalIPByIPConfig;
+
+/** 本机 IP 地址获取器（仅限 Windows）。*/
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final public class IPv4Converter
+{
+    /** IPV4 String -> UINT32 */
+    public static Long ipToLong(@NotNull String ip)
+    {
+        // 如果配置中填了 localhost，
+        // 我们便直接调用 ipconfig 拿到本机 IP（Linux 不适用）
+        if (ip.equals("localhost")) {
+            ip = getLocalIPByIPConfig();
+        }
+
+        String[] parts = ip.split("\\.");
+
+        if (parts.length != 4)
+        {
+            throw new
+            IllegalArgumentException(
+                String.format("Invalid IPv4 address: %s", ip)
+            );
+        }
+
+        long result = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int octet = Integer.parseInt(parts[i]);
+            result    = (result << 8) | octet;
+        }
+
+        return result;
+    }
+
+    /** UINT32 -> IPV4 String */
+    @Contract(pure = true)
+    public static @NotNull String longToIp(long ip)
+    {
+        return
+            String.format(
+                "%d.%d.%d.%d",
+                (ip >> 24) & 0xFF,
+                (ip >> 16) & 0xFF,
+                (ip >> 8) & 0xFF,
+                ip & 0xFF
+            );
+    }
+}
