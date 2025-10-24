09d0da6c023dd5b8b98fd87d583feb82f58ba779
浩进 朱 <3191955858@qq.com>
2025-10-24 15:52:13 +0800

    来自配置文件的 RabbitMQ 属性类。

---
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/properties/RabbitMQProperties.java
@@ -0,0 +1,27 @@
+package com.jesse.indicator_receiver.properties;
+
+// import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/** 来自配置文件的 RabbitMQ 属性类。*/
+@Data
+@ToString
+@Component
+@ConfigurationProperties(prefix = "app.rabbitmq")
+public class RabbitMQProperties
+{
+    private String host;
+    private int    port;
+    private String user;
+    private String password;
+    private String virtualHost;
+    private int    connectTimeout;
+
+//    @PostConstruct
+//    private void showProperties() {
+//        System.out.println(this);
+//    }
+}
