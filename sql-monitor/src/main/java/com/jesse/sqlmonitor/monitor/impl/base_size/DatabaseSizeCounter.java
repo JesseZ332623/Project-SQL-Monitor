35448ca6774bf2f6dc4251f2733a3d8b1c3ec0a8
浩进 朱 <3191955858@qq.com>
2025-10-24 15:59:10 +0800

    数据库大小计算器接口。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/monitor/impl/base_size/DatabaseSizeCounter.java
@@ -0,0 +1,14 @@
+package com.jesse.sqlmonitor.monitor.impl.base_size;
+
+import com.jesse.sqlmonitor.constants.QueryOrder;
+import com.jesse.sqlmonitor.response_body.DatabaseSize;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+/** 数据库大小计算器接口。*/
+public interface DatabaseSizeCounter
+{
+    Mono<Map<String, DatabaseSize>>
+    getDatabaseSizeInfo(String schemaName, QueryOrder queryOrder);
+}
