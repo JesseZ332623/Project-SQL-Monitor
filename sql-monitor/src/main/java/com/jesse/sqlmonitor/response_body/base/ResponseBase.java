77155c1ae12739601fa66b6e962689c8cd5daf4e
浩进 朱 <3191955858@qq.com>
2025-10-24 16:10:48 +0800

    所有响应体的基类，仅作标记。（在 Jackson 序列化/反序列化时有大用，作为类型令牌传入）

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/response_body/base/ResponseBase.java
@@ -0,0 +1,26 @@
+package com.jesse.sqlmonitor.response_body.base;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.jesse.sqlmonitor.response_body.*;
+import com.jesse.sqlmonitor.response_body.qps.ExtremeQPS;
+import com.jesse.sqlmonitor.response_body.qps.QPSResult;
+import com.jesse.sqlmonitor.response_body.qps.StandingDeviationQPS;
+import lombok.*;
+
+/** 所有响应体的基类，仅作标记。（在 Jackson 序列化/反序列化时有大用，作为类型令牌传入）*/
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = ConnectionUsage.class, name = "connectionUsage"),
+    @JsonSubTypes.Type(value = DatabaseSize.class,    name = "databaseSize"),
+    @JsonSubTypes.Type(value = InnodbBufferCacheHitRate.class, name = "innodbBufferCacheHitRate"),
+    @JsonSubTypes.Type(value = NetWorkTraffic.class, name = "networkTraffic"),
+    @JsonSubTypes.Type(value = QPSResult.class,      name = "qps"),
+    @JsonSubTypes.Type(value = ExtremeQPS.class,     name = "extreme-qps"),
+    @JsonSubTypes.Type(value = StandingDeviationQPS.class, name = "stddev-qps")
+})
+public abstract class ResponseBase<T extends ResponseBase<T>> {}
