90040056ad112c0a1d1f044ebcd5e1b328de43bf
浩进 朱 <3191955858@qq.com>
2025-10-24 16:06:39 +0800

    监控日志数据操作服务接口。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/indicator_record/service/MonitorLogService.java
@@ -0,0 +1,17 @@
+package com.jesse.sqlmonitor.indicator_record.service;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/** 监控日志数据操作服务接口。*/
+public interface MonitorLogService
+{
+    /** 读取某个时间点之前的指定 IP 下所有指定类型的监控日志记录（按时间排序）。*/
+    Mono<ServerResponse>
+    fetchIndicatorLog(ServerRequest request);
+
+    /** 计算某个时间点之前的指定 IP 下的 QPS 统计数据。*/
+    Mono<ServerResponse>
+    qpsStatistics(ServerRequest request);
+}
