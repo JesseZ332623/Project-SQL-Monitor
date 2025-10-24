b0243b9f957290ad8bb0a93f029ff81d81b1fab9
浩进 朱 <3191955858@qq.com>
2025-10-24 16:00:34 +0800

    指标统计数据查询端点配置类。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/route/endpoints_config/IndicatorQueryEndpoints.java
@@ -0,0 +1,14 @@
+package com.jesse.sqlmonitor.route.endpoints_config;
+
+/** 指标统计数据查询端点配置类。*/
+public class IndicatorQueryEndpoints
+{
+    public static final
+    String ROOT = "/api/indicator";
+
+    public static final
+    String MONITOR_LOG_QUERY = ROOT + "/log";
+
+    public static final
+    String QPS_STATISTICS = ROOT + "/qps-statistics";
+}
