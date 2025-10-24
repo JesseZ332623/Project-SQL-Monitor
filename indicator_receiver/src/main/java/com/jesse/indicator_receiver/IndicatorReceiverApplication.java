4433881255672f314446ed9909a10b4f10307c80
浩进 朱 <3191955858@qq.com>
2025-10-24 15:46:31 +0800

    指标接收器服务启动类。

---
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/IndicatorReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.jesse.indicator_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 指标接收器服务启动类。*/
+@SpringBootApplication
+public class IndicatorReceiverApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(IndicatorReceiverApplication.class, args);
+	}
+}
