3ab8ff441f9285ec4e01aef743b0d8e82c0b9a7c
浩进 朱 <3191955858@qq.com>
2025-10-24 16:10:56 +0800

    携带数据库地址 + 时间戳 + 指标数据的响应

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/response_body/SentIndicator.java
@@ -0,0 +1,22 @@
+package com.jesse.sqlmonitor.response_body;
+
+import com.jesse.sqlmonitor.response_body.base.ResponseBase;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/** 携带数据库地址 + 时间戳 + 指标数据的响应
+ *（通过 {@link reactor.rabbitmq.Sender}）发送出去。
+ */
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class SentIndicator<T extends ResponseBase<T>>
+    extends ResponseBase<T>
+{
+    private LocalDateTime localDateTime;
+    private String        address;
+    private T             indicator;
+}
