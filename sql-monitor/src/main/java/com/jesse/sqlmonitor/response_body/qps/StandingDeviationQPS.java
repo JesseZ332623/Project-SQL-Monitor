fdc3f97104bfdf04a07e247241c250722ba152ec
浩进 朱 <3191955858@qq.com>
2025-10-24 16:12:59 +0800

    指定条件下的 QPS 标准差和负载均衡率响应。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/response_body/qps/StandingDeviationQPS.java
@@ -0,0 +1,32 @@
+package com.jesse.sqlmonitor.response_body.qps;
+
+import com.jesse.sqlmonitor.response_body.base.ResponseBase;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+/** 指定条件下的 QPS 标准差和负载均衡率响应。*/
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Schema(
+    description = "指定条件下的 QPS 标准差和负载均衡率",
+    requiredProperties = {
+        "stddev",
+        "loadStability",
+        "dataPoints"
+    }
+)
+public class StandingDeviationQPS extends ResponseBase<StandingDeviationQPS>
+{
+    @Schema(description = "QPS 标准差", example = "66.63315972948297")
+    private double stddev;
+
+    @Schema(description = "QPS 负载均衡率", examples = "3.655882882369868163")
+    private double loadStability;
+
+    @Schema(description = "数据点采样数", example = "2722")
+    private long dataPoints;
+}
