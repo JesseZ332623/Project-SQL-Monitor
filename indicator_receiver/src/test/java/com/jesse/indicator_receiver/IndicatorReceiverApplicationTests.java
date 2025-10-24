ab7b2953e25fcb80418d5687b62ff86cbab19bc9
浩进 朱 <3191955858@qq.com>
2025-10-24 15:46:59 +0800

    指标接收器服务测试类（暂无测试）。

---
+++ indicator_receiver/src/test/java/com/jesse/indicator_receiver/IndicatorReceiverApplicationTests.java
@@ -0,0 +1,7 @@
+package com.jesse.indicator_receiver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/** 指标接收器服务测试类。*/
+@SpringBootTest
+class IndicatorReceiverApplicationTests {}
