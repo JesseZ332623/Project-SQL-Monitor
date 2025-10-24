7e5488ee91c5498d21f0cf824372608125417217
浩进 朱 <3191955858@qq.com>
2025-10-24 16:06:06 +0800

    监控指标实体。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/indicator_record/entity/MonitorLog.java
@@ -0,0 +1,34 @@
+package com.jesse.sqlmonitor.indicator_record.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+/** 监控指标实体。*/
+@Data
+@ToString
+@Builder(builderClassName = "Builder")
+@Table(name = "monitor_log")
+public class MonitorLog
+{
+    @Id
+    @Column("log_id")
+    private Long logId;
+
+    @Column("datetime")
+    private LocalDateTime datetime;
+
+    @Column("server_ip")
+    private Long serverIP;
+
+    @Column("indicator")
+    private String indicator;
+
+    @Column("indicator_type")
+    private IndicatorType indicatorType;
+}
