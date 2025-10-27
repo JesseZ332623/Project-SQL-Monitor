08a7647ab378cbfca6d21d7e8eeb89ae5fcbc4ee
浩进 朱 <3191955858@qq.com>
2025-10-27 13:52:10 +0800

    指标监控相关常量。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/monitor/constants/MonitorConstants.java
@@ -0,0 +1,17 @@
+package com.jesse.sqlmonitor.monitor.constants;
+
+/** 指标监控相关常量。*/
+final public class MonitorConstants
+{
+    /** 指标计算最多重试次数。*/
+    public static final
+    int MAX_RETRIES = 10;
+
+    /** 两次查询间隔的最小时间（单位：毫秒）*/
+    public static final
+    long MIN_TIME_DIFF_MS = 500L;
+
+    /** 前 2 回的快照结果需要忽略。*/
+    public static final
+    int IGNORE_SNAPSHOTS = 2;
+}
