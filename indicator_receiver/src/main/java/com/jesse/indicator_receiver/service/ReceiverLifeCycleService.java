2fd346c0198dcfa7252d2129cf475a78d544dcc5
浩进 朱 <3191955858@qq.com>
2025-11-03 10:07:20 +0800

    指标数据接收器手动生命周期管理服务接口。

--- indicator_receiver/src/main/java/com/jesse/indicator_receiver/service/ReceiverLifeCycleService.java
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/service/ReceiverLifeCycleService.java
@@ -7,7 +7,12 @@ import reactor.core.publisher.Mono;
 /** 指标数据接收器手动生命周期管理服务接口。*/
 public interface ReceiverLifeCycleService
 {
+    /** 获取指标数据消费者运行状态。*/
     Mono<ServerResponse> start(ServerRequest request);
+
+    /** 手动启动指标数据消费者。*/
     Mono<ServerResponse> stop(ServerRequest request);
+
+    /** 手动关闭指标数据消费者。*/
     Mono<ServerResponse> runStatus(ServerRequest request);
 }
