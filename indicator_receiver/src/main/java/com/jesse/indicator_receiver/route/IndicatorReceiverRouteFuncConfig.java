d40eb26c8ad2c45c6d5c7e96733b831d36a7e5ad
浩进 朱 <3191955858@qq.com>
2025-11-03 10:06:48 +0800

    指标数据接收器服务路由函数配置。

--- indicator_receiver/src/main/java/com/jesse/indicator_receiver/route/IndicatorReceiverRouteFuncConfig.java
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/route/IndicatorReceiverRouteFuncConfig.java
@@ -1,9 +1,14 @@
 package com.jesse.indicator_receiver.route;
 
 import com.jesse.indicator_receiver.service.ReceiverLifeCycleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.jetbrains.annotations.NotNull;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -15,6 +20,61 @@ import static com.jesse.indicator_receiver.route.IndicatorReceiverEndpoints.*;
 public class IndicatorReceiverRouteFuncConfig
 {
     @Bean
+    @RouterOperations({
+        @RouterOperation(
+            path      = RUN_STATUS,
+            method    = { RequestMethod.GET },
+            operation = @Operation(
+                operationId = "runStatus",
+                summary     = "获取指标数据消费者运行状态",
+                tags        = {"指标数据运行状态获取"},
+                responses   = {
+                    @ApiResponse(
+                        responseCode = "200",
+                        description  = "指标数据运行状态（RUNNING 或者 STOPPING）"
+                    )
+                }
+            )
+        ),
+        @RouterOperation(
+            path      = START_RECEIVER,
+            method    = { RequestMethod.POST },
+            operation = @Operation(
+                operationId = "start",
+                summary     = "手动启动指标数据消费者",
+                tags        = {"手动启动指标数据消费者"},
+                responses   =  {
+                    @ApiResponse(
+                        responseCode = "200",
+                        description  = "成功启动指标消费者"
+                    ),
+                    @ApiResponse(
+                        responseCode = "400",
+                        description  = "重复启动指标数据消费者"
+                    )
+                }
+            )
+        ),
+        @RouterOperation(
+            path      = STOP_RECEIVER,
+            method    = { RequestMethod.POST },
+            operation = @Operation(
+                operationId = "stop",
+                summary     = "手动关闭指标数据消费者",
+                tags        = {"手动关闭指标数据消费者"},
+                responses   =  {
+                    @ApiResponse(
+                        responseCode = "200",
+                        description  = "成功关闭指标消费者"
+                    ),
+                    @ApiResponse(
+                        responseCode = "400",
+                        description  = "重复关闭指标数据消费者"
+                    )
+                }
+            )
+        )
+    })
     public RouterFunction<ServerResponse>
     indicatorReceiverRouteFunc(
         @NotNull
