b52d9f5f485859ba3b243cd80ccdb6fc38801c6a
浩进 朱 <3191955858@qq.com>
2025-11-06 09:13:33 +0800

    IPv4 转换器。

--- indicator_receiver/src/main/java/com/jesse/indicator_receiver/utils/IPv4Converter.java
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/utils/IPv4Converter.java
@@ -5,8 +5,6 @@ import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import static com.jesse.indicator_receiver.utils.SystemIPv4Getter.getLocalIPByIPConfig;
-
 /** IPv4 转换器。*/
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 final public class IPv4Converter
@@ -17,7 +15,7 @@ final public class IPv4Converter
         // 如果配置中填了 localhost，
         // 我们便直接调用 ipconfig 拿到本机 IP（Linux 不适用）
         if (ip.equals("localhost")) {
-            ip = getLocalIPByIPConfig();
+            ip = SystemIPv4Getter.getLocalHost();
         }
 
         String[] parts = ip.split("\\.");
