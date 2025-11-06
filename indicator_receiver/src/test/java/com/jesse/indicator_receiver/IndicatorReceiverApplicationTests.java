82a7140e6f52e8954ea6c5cc91dde784a1502a5a
浩进 朱 <3191955858@qq.com>
2025-11-06 09:13:11 +0800

    指标接收器服务测试类。

--- indicator_receiver/src/test/java/com/jesse/indicator_receiver/IndicatorReceiverApplicationTests.java
+++ indicator_receiver/src/test/java/com/jesse/indicator_receiver/IndicatorReceiverApplicationTests.java
@@ -1,7 +1,24 @@
 package com.jesse.indicator_receiver;
 
+import com.jesse.indicator_receiver.utils.SystemIPv4Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /** 指标接收器服务测试类。*/
+@Slf4j
 @SpringBootTest
-class IndicatorReceiverApplicationTests {}
+class IndicatorReceiverApplicationTests
+{
+    @Test
+    public void getLocalIPByIPConfig() throws UnknownHostException {
+//        System.out.println(
+//            SystemIPv4Getter.getLocalIPByIPConfig()
+//        );
+
+        System.out.println(InetAddress.getLocalHost().getHostAddress());
+    }
+}
