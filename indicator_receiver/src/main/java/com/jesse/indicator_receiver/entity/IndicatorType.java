abd694ddd7adc02fe3be15a6241cdc5ce2cae54a
浩进 朱 <3191955858@qq.com>
2025-11-06 13:59:29 +0800

    数据库指标类型枚举。

--- indicator_receiver/src/main/java/com/jesse/indicator_receiver/entity/IndicatorType.java
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/entity/IndicatorType.java
@@ -3,9 +3,18 @@ package com.jesse.indicator_receiver.entity;
 /** 数据库指标类型枚举。*/
 public enum IndicatorType
 {
+    /** 连接使用指标类型。 */
     ConnectionUsage,
+
+    /** 库/表 大小指标类型。（似乎暂时没有用到）*/
     DatabaseSize,
+
+    /** InnoDB 缓存命中率指标类型。*/
     InnodbBufferCacheHitRate,
+
+    /** 网络流量指标类型。*/
     NetWorkTraffic,
+
+    /** QPS 指标类型。*/
     QPSResult
 }
