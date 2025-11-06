260ed2d27416352abbf475c2a928f5acd7868da8
浩进 朱 <3191955858@qq.com>
2025-11-06 09:13:45 +0800

    本机 IP 地址获取器。

--- indicator_receiver/src/main/java/com/jesse/indicator_receiver/utils/SystemIPv4Getter.java
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/utils/SystemIPv4Getter.java
@@ -2,78 +2,31 @@ package com.jesse.indicator_receiver.utils;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-/** 本机 IP 地址获取器（仅限 Windows）。*/
+/** 本机 IP 地址获取器。*/
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 final public class SystemIPv4Getter
 {
-    private static final Pattern IP_PATTERN
-        = Pattern.compile("IPv4 地址[^:]*: ([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})");
+    private static final String FULLBACK_IP = "127.0.0.1";
 
-    /** 自动专用 IP 寻址地址段。*/
-    private static final
-    String APIPA_PREFIX = "169.254";
-
-    /** 回环地址。*/
-    private static final
-    String LOOPBACK = "127.0.0.1";
-
-    public static @NotNull String getLocalIPByIPConfig()
+    public static @NotNull String
+    getLocalHost()
     {
-        Process process       = null;
-        BufferedReader reader = null;
         try
         {
-            ProcessBuilder processBuilder = new ProcessBuilder("ipconfig");
-            process = processBuilder.start();
-
-            reader
-                = new BufferedReader(
-                    new InputStreamReader(process.getInputStream(), "GBK")
-                );
-
-            String line;
-
-            while ((line = reader.readLine()) != null)
-            {
-                Matcher matcher = IP_PATTERN.matcher(line);
-
-                if (matcher.find())
-                {
-                    String ip = matcher.group(1);
-                    if (!ip.startsWith(APIPA_PREFIX) && !ip.equals(LOOPBACK)) {
-                        return ip;
-                    }
-                }
-            }
+            return
+            InetAddress.getLocalHost().getHostAddress();
         }
-        catch (Exception exception) {
-            exception.printStackTrace(System.err);
-        }
-        finally
+        catch (UnknownHostException unknownHostException)
         {
-            try
-            {
-                if (Objects.nonNull(reader)) {
-                    reader.close();
-                }
-                if (Objects.nonNull(process)) {
-                    process.waitFor();
-                }
-            }
-            catch (Exception exception)
-            {
-                exception.printStackTrace(System.err);
-            }
+            log.warn("{}", unknownHostException.getMessage());
+            return FULLBACK_IP;
         }
-
-        return "127.0.0.1";
     }
 }
