c127012d41491c397fa8c00b75b7be2f9c04d1a3
浩进 朱 <3191955858@qq.com>
2025-11-14 13:52:42 +0800

    表示一个随机笑话响应体的 DTO。

---
+++ sql-monitor/src/test/java/com/jesse/sqlmonitor/dto/RandomJokeDTO.java
@@ -0,0 +1,17 @@
+package com.jesse.sqlmonitor.dto;
+
+import lombok.*;
+
+/** 表示一个随机笑话响应体的 DTO。*/
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class RandomJokeDTO
+{
+    private String type;
+    private String setup;
+    private String punchline;
+    private Long   id;
+}
