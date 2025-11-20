d30e7fd070d55ce869961ae97ba571f079da65d7
Jesse <101250851+JesseZ332623@users.noreply.github.com>
2025-11-20 20:25:27 +0800

    指标数据接收器手动生命周期管理服务测试（未完成）。 (#18)

---
+++ indicator_receiver/src/test/java/com/jesse/indicator_receiver/ReceiverLifeCycleServiceTests.java
@@ -0,0 +1,14 @@
+package com.jesse.indicator_receiver;
+
+import com.jesse.indicator_receiver.service.impl.ReceiverLifeCycleServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 指标数据接收器手动生命周期管理服务
+ * {@link ReceiverLifeCycleServiceImpl} 测试。（未完成）
+ */
+@Slf4j
+@SpringBootTest
+public class ReceiverLifeCycleServiceTests {
+}
