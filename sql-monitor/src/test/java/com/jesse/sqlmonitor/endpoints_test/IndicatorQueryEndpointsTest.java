0350fd7a7cbb70a5d3362914a40b6e63d44cddec
浩进 朱 <3191955858@qq.com>
2025-11-24 18:02:40 +0800

    指标统计数据查询端点测试。（未完成）

---
+++ sql-monitor/src/test/java/com/jesse/sqlmonitor/endpoints_test/IndicatorQueryEndpointsTest.java
@@ -0,0 +1,10 @@
+package com.jesse.sqlmonitor.endpoints_test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/** 指标统计数据查询端点测试。*/
+@Slf4j
+@SpringBootTest
+public class IndicatorQueryEndpointsTest {
+}
