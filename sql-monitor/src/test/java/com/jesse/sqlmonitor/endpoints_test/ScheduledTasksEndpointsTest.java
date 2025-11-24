2777cdccb025200e760039703a4498a3d2949ec2
浩进 朱 <3191955858@qq.com>
2025-11-24 18:02:49 +0800

    手动执行定时任务端点测试。（未完成）

---
+++ sql-monitor/src/test/java/com/jesse/sqlmonitor/endpoints_test/ScheduledTasksEndpointsTest.java
@@ -0,0 +1,10 @@
+package com.jesse.sqlmonitor.endpoints_test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/** 手动执行定时任务端点测试。*/
+@Slf4j
+@SpringBootTest
+public class ScheduledTasksEndpointsTest {
+}
