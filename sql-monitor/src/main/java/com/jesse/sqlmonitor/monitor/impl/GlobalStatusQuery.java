71570b0404138fca41e77178d40c1ae5da642fe7
浩进 朱 <3191955858@qq.com>
2025-11-24 16:33:10 +0800

    数据库全局状态查询器（新增 查询所有的数据库名 接口）。

--- sql-monitor/src/main/java/com/jesse/sqlmonitor/monitor/impl/GlobalStatusQuery.java
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/monitor/impl/GlobalStatusQuery.java
@@ -10,6 +10,7 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.jesse.sqlmonitor.utils.SQLMonitorUtils.tryGetNumericValue;
@@ -28,6 +29,34 @@ public class GlobalStatusQuery
     )
     {
         this.databaseClient = databaseClient;
+    }
+
+    /**
+     * 查询所有的数据库名
+     *（用 includeSysSchema 标志位指定是否包含系统数据库）。
+     */
+    public Mono<List<String>>
+    getAllSchemaName(boolean includeSysSchema)
+    {
+        final StringBuilder querySQL = new StringBuilder(
+            "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA"
+        );
+
+        if (!includeSysSchema)
+        {
+            querySQL.append(
+                " WHERE SCHEMA_NAME NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys')"
+            );
+        }
+
+        return
+        this.databaseClient
+            .sql(querySQL.toString())
+            .fetch()
+            .all()
+            .map((shemaNameMap) ->
+                (String) shemaNameMap.get("SCHEMA_NAME"))
+            .collectList();
     }
 
     /** 查询本数据库指定全局状态。*/
