35e0d06de186e65316af841c05d8ea8250e2be59
浩进 朱 <3191955858@qq.com>
2025-11-25 15:47:55 +0800

    Redisson 分布式锁简单测试。

--- sql-monitor/src/test/java/com/jesse/sqlmonitor/component_test/RedissonTest.java
+++ sql-monitor/src/test/java/com/jesse/sqlmonitor/component_test/RedissonTest.java
@@ -48,6 +48,19 @@ public class RedissonTest
 
     private RedissonReactiveClient redissonReactiveClient = null;
 
+    /** 挂上了代理的 HTTP 客户端实例。*/
+    private final
+    WebClient webClient
+        = WebClient.builder()
+        .clientConnector(new ReactorClientHttpConnector(
+            HttpClient.create()
+                .proxy((proxy) ->
+                    proxy.type(ProxyProvider.Proxy.SOCKS5)
+                        .host("localhost")
+                        .port(10808)
+                )
+        )).build();
+
     public RedissonReactiveClient
     getRedissonReactiveClient()
     {
@@ -72,19 +85,6 @@ public class RedissonTest
 
         return this.redissonReactiveClient;
     }
-
-    /** 挂上了代理的 HTTP 客户端实例。*/
-    private final
-    WebClient webClient
-        = WebClient.builder()
-            .clientConnector(new ReactorClientHttpConnector(
-                HttpClient.create()
-                    .proxy((proxy) ->
-                        proxy.type(ProxyProvider.Proxy.SOCKS5)
-                            .host("localhost")
-                            .port(10808)
-                    )
-            )).build();
 
     private @NotNull Mono<String> getDatabaseTime()
     {
@@ -128,7 +128,7 @@ public class RedissonTest
         final long contextThreadId = IdUtil.getSnowflakeNextId();
 
         Flux.interval(Duration.ZERO, Duration.ofMillis(1500L))
-            .take(10L)
+            .take(5L)
             .concatMap((sequence) ->
                 Mono.fromRunnable(() -> System.out.println("No. " + sequence))
                     .then(
