336c29f0b213b9924d4092467b1a4e442307d948
浩进 朱 <3191955858@qq.com>
2025-11-28 14:33:53 +0800

    MySQL 监视器应用启动类。（启用定时任务）

--- sql-monitor/src/main/java/com/jesse/sqlmonitor/SQLMonitorApplication.java
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/SQLMonitorApplication.java
@@ -2,8 +2,10 @@ package com.jesse.sqlmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /** MySQL 监视器应用启动类。*/
+@EnableScheduling
 @SpringBootApplication
 public class SQLMonitorApplication
 {
