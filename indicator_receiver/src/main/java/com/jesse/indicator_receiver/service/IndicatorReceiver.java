e4c9b75143bf130d0dc3179f5a16cd95e75c7abb
浩进 朱 <3191955858@qq.com>
2025-12-01 16:22:32 +0800

    指标数据接收器接口。（新增 getCountDownLatch() 接口）

--- indicator_receiver/src/main/java/com/jesse/indicator_receiver/service/IndicatorReceiver.java
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/service/IndicatorReceiver.java
@@ -1,12 +1,22 @@
 package com.jesse.indicator_receiver.service;
 
+import com.jesse.indicator_receiver.service.impl.ReceiverLifecycleManager;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CountDownLatch;
 
 /** 指标数据接收器接口。*/
 public interface IndicatorReceiver
 {
     /** 设置是否正在运行的原子标志位。*/
     void setRunningFlag(boolean flag);
+
+    /**
+     * 获取指标接收器的 CountDownLatch 实例，
+     * 供 {@link ReceiverLifecycleManager#stop()} 方法中尝试使用它
+     * 来等待所有批量插入操作完成。
+     */
+    CountDownLatch getCountDownLatch();
 
     Mono<Void> receiveIndicator();
 }
