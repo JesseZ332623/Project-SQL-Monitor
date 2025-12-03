2284b9f881028a99e39e222e2042c088817177e3
Jesse <101250851+JesseZ332623@users.noreply.github.com>
2025-12-03 09:32:10 +0800

    拥有更全面错误处理的邮箱服务授权码获取实现。 (#31)
    * 邮箱服务授权码获取实现。
    
    - 完善 `get()` 方法，令其拥有更完善的错误处理能力，在读取不到指定邮箱的授权码时直接终止整个应用。
    
    * 简介

--- sql-monitor/src/main/java/com/jesse/sqlmonitor/config/EmailAuthCodeConfig.java
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/config/EmailAuthCodeConfig.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.core.publisher.Mono;
 
-import java.util.Map;
+import java.time.Duration;
 import java.util.Objects;
+
+import static java.lang.String.format;
 
 /** 邮箱服务授权码获取实现。*/
 @Slf4j
@@ -27,6 +30,11 @@ public class EmailAuthCodeConfig implements EmailServiceAuthCodeGetter
         this.databaseClient = databaseClient;
     }
 
+    /**
+     * 获取邮箱授权码的实现。
+     *
+     * @throws IllegalStateException 查询不到授权码或者授权码为空时抛出，直接终止整个应用。
+     */
     @Override
     public String get()
     {
@@ -40,22 +48,32 @@ public class EmailAuthCodeConfig implements EmailServiceAuthCodeGetter
                 email = :email
             """;
 
-        Map<String, Object> authCodeResult
-            = this.databaseClient
-                  .sql(authCodeGetterSQL)
-                  .bind("email", senderEmail)
-                  .fetch()
-                  .one()
-                  .doOnError((error) ->
-                      log.error(
-                          "Get email auth code for {} failed, Caused by: {}",
-                          senderEmail, error.getMessage()
-                      ))
-                  .block();
-
         return
-        (String)
-        Objects.requireNonNull(authCodeResult)
-               .get("email_auth_code");
+        this.databaseClient
+            .sql(authCodeGetterSQL)
+            .bind("email", senderEmail)
+            .fetch()
+            .one()
+            .map((result) ->
+                (String) result.get("email_auth_code"))
+            .timeout(Duration.ofSeconds(1L))
+            .switchIfEmpty(
+                Mono.error(
+                    new IllegalStateException(
+                        format("Email auth code of: %s is not found!", senderEmail)
+                    )
+                )
+            )
+            .filter((authCode) ->
+                (Objects.nonNull(authCode) && !authCode.trim().isEmpty()))
+            .switchIfEmpty(
+                Mono.error(
+                    new IllegalStateException(
+                        format("Email auth code of: %s is null or empty!", senderEmail)
+                    )
+                )
+            )
+            .onErrorResume(Mono::error)
+            .block();
     }
 }
