c1a418061f1d0b6046ce405677bcf8e5d98bd830
浩进 朱 <3191955858@qq.com>
2025-12-09 13:38:17 +0800

    指标数据发送器实现。
    - 更清晰的逻拆分

--- sql-monitor/src/main/java/com/jesse/sqlmonitor/indicator_record/service/impl/IndicatorSenderImpl.java
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/indicator_record/service/impl/IndicatorSenderImpl.java
@@ -1,5 +1,6 @@
 package com.jesse.sqlmonitor.indicator_record.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jesse.sqlmonitor.response_body.SentIndicator;
 import com.jesse.sqlmonitor.response_body.base.ResponseBase;
@@ -7,6 +8,8 @@ import com.jesse.sqlmonitor.indicator_record.exception.RecordIndicatorFailed;
 import com.jesse.sqlmonitor.indicator_record.service.IndicatorSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Contract;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.rabbitmq.OutboundMessage;
@@ -22,10 +25,12 @@ import java.time.Duration;
 public class IndicatorSenderImpl implements IndicatorSender
 {
     /** 交换机名。*/
-    private final static String EXCHANGE = "sql-monitor-exchange";
+    private final static
+    String EXCHANGE = "sql-monitor-exchange";
 
     /** 路由键名。*/
-    private final static String ROUTING_KEY = "sql.monitor";
+    private final static
+    String ROUTING_KEY = "sql.monitor";
 
     /** RabbitMQ 消息发送器。*/
     private final Sender sender;
@@ -33,31 +38,36 @@ public class IndicatorSenderImpl implements IndicatorSender
     /** Jackson JSON 解析器。*/
     private final ObjectMapper mapper;
 
-    /** 向 RabbitMQ 发送指标数据。*/
-    public <T extends ResponseBase<T>>
-    Mono<Void> sendIndicator(SentIndicator<T> indicator)
+    /** 将消息绑定到指定的交换机和路由键。*/
+    @Contract("_ -> new")
+    private <T extends ResponseBase<T>>
+    @NonNull OutboundMessage
+    createMessage(@NonNull SentIndicator<T> indicator) throws JsonProcessingException
     {
         return
-        Mono.fromCallable(() -> {
-            byte[] indicatorBytes
-                = this.mapper
-                      .writeValueAsString(indicator)
-                      .getBytes(StandardCharsets.UTF_8);
+        new OutboundMessage(
+            EXCHANGE, ROUTING_KEY,
+            this.mapper
+                .writeValueAsString(indicator)
+                .getBytes(StandardCharsets.UTF_8)
+            );
+    }
 
-            return new
-            OutboundMessage(EXCHANGE, ROUTING_KEY, indicatorBytes);
-        })
-        .flatMap((message) ->
-            this.sender
-                .send(Mono.just(message))
-                .then())
-        .timeout(Duration.ofSeconds(5L))
-        .onErrorResume((exception) ->
-            Mono.error(
-                new RecordIndicatorFailed(
-                    exception.getMessage(), exception
-                )
-            )
-        );
+    /** 向 RabbitMQ 发送指标数据。*/
+    @Override
+    public <T extends ResponseBase<T>> Mono<Void>
+    sendIndicator(@NonNull SentIndicator<T> indicator)
+    {
+        return
+        this.sender
+            .send(Mono.fromCallable(() -> this.createMessage(indicator)))
+            .then()
+            .timeout(Duration.ofSeconds(5L))
+            .onErrorMap(
+                (exception) ->
+                    new RecordIndicatorFailed(
+                        exception.getMessage(), exception
+                    )
+            );
     }
 }
