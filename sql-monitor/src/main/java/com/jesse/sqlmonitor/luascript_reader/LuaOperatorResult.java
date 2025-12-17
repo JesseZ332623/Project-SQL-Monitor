9356e5f770312db2424c593b82e3d1d3f8152406
浩进 朱 <3191955858@qq.com>
2025-12-17 17:00:17 +0800

    Lua 脚本的执行结果。

--- sql-monitor/src/main/java/com/jesse/sqlmonitor/luascript_reader/LuaOperatorResult.java
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/luascript_reader/LuaOperatorResult.java
@@ -1,15 +1,16 @@
 package com.jesse.sqlmonitor.luascript_reader;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /** Lua 脚本的执行结果是一个 JSON 类型，会被映射成该 POJO。*/
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@ToString
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 public class LuaOperatorResult
 {
     /** Lua 脚本执行状态。*/
-    private String status;
+    private final String status;
+
+    /** Lua 脚本执行完成时的时间戳。*/
+    private final long timestamp;
 }
