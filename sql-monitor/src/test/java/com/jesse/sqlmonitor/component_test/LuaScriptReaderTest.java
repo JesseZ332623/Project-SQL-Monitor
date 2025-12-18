5765f8cbb447903fabf39f9593ecc60ee1a47198
浩进 朱 <3191955858@qq.com>
2025-12-18 11:07:27 +0800

    LuaScriptReader 简单测试，旨在展示读取器和 Lua 脚本的相关标准用法。

--- sql-monitor/src/test/java/com/jesse/sqlmonitor/component_test/LuaScriptReaderTest.java
+++ sql-monitor/src/test/java/com/jesse/sqlmonitor/component_test/LuaScriptReaderTest.java
@@ -4,6 +4,9 @@ import cn.hutool.core.util.RandomUtil;
 import com.jesse.sqlmonitor.luascript_reader.LuaOperatorResult;
 import com.jesse.sqlmonitor.luascript_reader.LuaScriptOperatorType;
 import com.jesse.sqlmonitor.luascript_reader.LuaScriptReader;
+import com.jesse.sqlmonitor.properties.R2dbcMasterProperties;
+import com.jesse.sqlmonitor.properties.RedisCacheProperties;
+import com.jesse.sqlmonitor.utils.PrettyJSONPrinter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,20 +15,28 @@ import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 
 /**
  * {@link LuaScriptReader} 简单测试，
- * 旨在展示标准用法和 Redis Lua 脚本对 CJSON 的支持。
+ * 旨在展示读取器和 Lua 脚本的相关标准用法。
  */
 @SpringBootTest
 public class LuaScriptReaderTest
 {
+    @Autowired
+    private R2dbcMasterProperties masterProperties;
+
+    @Autowired
+    private RedisCacheProperties cacheProperties;
+
     @Autowired
     private LuaScriptReader luaScriptReader;
 
     @Autowired
     private ReactiveRedisTemplate<String, LuaOperatorResult> scriptTemmplate;
 
+    /** 测试 Redis-Lua 脚本对 CJSON 的支持（终于不用手动拼接 JSON 字符串了）。*/
     @Test
     public void luaScriptReaderTest()
     {
@@ -33,7 +44,7 @@ public class LuaScriptReaderTest
             .take(5L)
             .concatMap((tick) ->
                 this.luaScriptReader
-                    .read(LuaScriptOperatorType.TEST_SCRIPT, "test-script.lua")
+                    .read(LuaScriptOperatorType.TEST_SCRIPT, "test-script-01.lua")
                     .flatMap((script) ->
                         this.scriptTemmplate
                             .execute(script, List.of(), RandomUtil.randomInt(100, 200))
@@ -50,5 +61,29 @@ public class LuaScriptReaderTest
                     .then()
             )
             .blockLast();
+    }
+
+    /** 测试 Redis-Lua 脚本如何处理 {@link Map} 类型的数据。*/
+    @Test
+    public void luaScriptReaderMapTest()
+    {
+        final String redisKey
+            = this.cacheProperties.getKeyPrefix() + this.masterProperties.getHost() + ":hash-map";
+
+        final Map<String, Object> testMap
+            = Map.of("name", "Jesse", "age", 114);
+
+        System.out.println(redisKey);
+
+        this.luaScriptReader
+            .read(LuaScriptOperatorType.TEST_SCRIPT, "test-script-02.lua")
+            .flatMap((script) ->
+                this.scriptTemmplate
+                    .execute(script, List.of(redisKey), testMap)
+                    .timeout(Duration.ofSeconds(1L))
+                    .next()
+                    .map(PrettyJSONPrinter::getPrettyFormatJSON)
+                    .doOnSuccess(System.out::println)
+            ).block();
     }
 }
