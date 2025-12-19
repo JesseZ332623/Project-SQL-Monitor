40254f3bbacdad265531f628decbd1fa2715f575
浩进 朱 <3191955858@qq.com>
2025-12-19 10:19:27 +0800

    非法 Lua 脚本路径异常。

--- sql-monitor/src/main/java/com/jesse/sqlmonitor/luascript_reader/impl/exception/LuaScriptSecurityException.java
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/luascript_reader/impl/exception/LuaScriptSecurityException.java
@@ -2,8 +2,6 @@ package com.jesse.sqlmonitor.luascript_reader.impl.exception;
 
 import lombok.Getter;
 
-import java.nio.file.Path;
-
 /** 非法 Lua 脚本路径异常。*/
 public class LuaScriptSecurityException extends RuntimeException
 {
