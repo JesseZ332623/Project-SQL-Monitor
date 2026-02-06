31440a4bb05964d129ffb0c1c4c14a129e096cff
浩进 朱 <3191955858@qq.com>
2026-02-06 11:26:38 +0800

    Lua 脚本读取器端点配置类。

---
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/route/endpoints_config/LuaScriptReaderEndpoints.java
@@ -0,0 +1,11 @@
+package com.jesse.sqlmonitor.route.endpoints_config;
+
+/** Lua 脚本读取器端点配置类。*/
+public class LuaScriptReaderEndpoints
+{
+    public static final
+    String ROOT         = "/api/lua-script";
+
+    public static final
+    String CACHE_CLEAR = "/cache-clear";
+}
