7769803ca683c6d33efd8c9b53daa49ff07e632f
浩进 朱 <3191955858@qq.com>
2026-02-06 10:25:13 +0800

    Redis Lua 脚本读取器接口。
    - 新增 cleanCache() 接口用于手动的清理脚本缓存。

--- sql-monitor/src/main/java/com/jesse/sqlmonitor/luascript_reader/LuaScriptReader.java
+++ sql-monitor/src/main/java/com/jesse/sqlmonitor/luascript_reader/LuaScriptReader.java
@@ -4,6 +4,7 @@ import com.jesse.sqlmonitor.luascript_reader.impl.LuaOperatorResult;
 import com.jesse.sqlmonitor.luascript_reader.impl.LuaScriptOperatorType;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import reactor.core.publisher.Mono;
 
 /** Redis Lua 脚本读取器接口。*/
@@ -18,6 +19,13 @@ public interface LuaScriptReader
      * @return 由 {@link DefaultRedisScript} 包装的，
      *         发布 Lua 脚本执行结果 {@link LuaOperatorResult} 的 {@link Mono}
      */
-    @NotNull Mono<DefaultRedisScript<LuaOperatorResult>>
+    @NotNull Mono<RedisScript<LuaOperatorResult>>
     read(LuaScriptOperatorType operatorType, String luaScriptName);
+
+    /**
+     * 清理所有缓存的 Lua 脚本。
+     *
+     * @return 清理调的缓存的脚本数量
+     */
+    Mono<Integer> cleanCache();
 }
