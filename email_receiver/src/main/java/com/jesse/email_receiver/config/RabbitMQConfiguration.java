72f0418f0b6878fd1e7ea16762a78b743553ffa4
浩进 朱 <3191955858@qq.com>
2026-02-25 13:42:29 +0800

    RabbitMQ 配置类。

---
+++ email_receiver/src/main/java/com/jesse/email_receiver/config/RabbitMQConfiguration.java
@@ -0,0 +1,54 @@
+package com.jesse.email_receiver.config;
+
+import com.jesse.email_receiver.properties.RabbitMQProperties;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.rabbitmq.*;
+
+/** RabbitMQ 配置类。*/
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfiguration
+{
+    /** 来自配置文件的 RabbitMQ 属性类。*/
+    private final RabbitMQProperties properties;
+
+    /** RabbitMQ 连接工厂配置。*/
+    @Bean(name = "RabbitMQConnectionFactory")
+    public @NotNull ConnectionFactory connectionFactory()
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost(properties.getHost());
+        factory.setPort(properties.getPort());
+        factory.setUsername(properties.getUser());
+        factory.setPassword(properties.getPassword());
+        factory.setVirtualHost(properties.getVirtualHost());
+        factory.setConnectionTimeout(properties.getConnectTimeout());
+
+        // 心跳和自动恢复配置
+        factory.setRequestedHeartbeat(60);
+        factory.setAutomaticRecoveryEnabled(true);
+        factory.setNetworkRecoveryInterval(5000);
+
+        return factory;
+    }
+
+    /** RabbitMQ 接收器配置。*/
+    @Bean
+    public Receiver
+    receiver(
+        @Qualifier("RabbitMQConnectionFactory")
+        ConnectionFactory factory
+    )
+    {
+        return
+        RabbitFlux.createReceiver(
+            new ReceiverOptions().connectionFactory(factory)
+        );
+    }
+}
