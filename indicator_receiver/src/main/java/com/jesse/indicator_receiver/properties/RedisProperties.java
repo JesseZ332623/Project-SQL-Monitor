88e3ab5d12a35fd7aca4514b7e0396b290e1d02a
浩进 朱 <3191955858@qq.com>
2026-03-05 10:08:02 +0800

    Redis 配置属性类。

---
+++ indicator_receiver/src/main/java/com/jesse/indicator_receiver/properties/RedisProperties.java
@@ -0,0 +1,19 @@
+package com.jesse.indicator_receiver.properties;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/** Redis 配置属性类。*/
+@Data
+@ToString
+@Component
+@ConfigurationProperties(prefix = "app.redis")
+public class RedisProperties
+{
+    private String host;
+    private int    port;
+    private String username;
+    private String password;
+}
