0c30b35fa4b625f14a23f84faacbc1a4e9378d2c
浩进 朱 <3191955858@qq.com>
2026-05-27 15:27:39 +0800

    ID 分配器服务路由函数配置类。

--- id_allocator/src/main/java/com/jesse/id_allocator/route/IdAllocatorRouteFunctionConfiguration.java
+++ id_allocator/src/main/java/com/jesse/id_allocator/route/IdAllocatorRouteFunctionConfiguration.java
@@ -1,9 +1,9 @@
 package com.jesse.id_allocator.route;
 
 import com.jesse.id_allocator.route.filter.MonitoringFilter;
+import com.jesse.id_allocator.service.IdAllocatorService;
 import com.jesse.id_allocator.service.ReplenisherLifecycleService;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -16,14 +16,18 @@ import static com.jesse.id_allocator.route.IdAllocatorEndpoints.*;
 
 /** ID 分配器服务路由函数配置类。*/
 @Configuration
+@RequiredArgsConstructor
 public class IdAllocatorRouteFunctionConfiguration
 {
+    private final
+    ReplenisherLifecycleService receiverLifeCycleService;
+
+    private final
+    IdAllocatorService idAllocatorService;
+
     @Bean
     public RouterFunction<ServerResponse>
-    indicatorReceiverRouteFunc(
-        @NotNull @Autowired
-        final ReplenisherLifecycleService receiverLifeCycleService
-    )
+    indicatorReceiverRouteFunc()
     {
         return
         RouterFunctions.nest(
@@ -31,6 +35,8 @@ public class IdAllocatorRouteFunctionConfiguration
                 .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
             RouterFunctions.route()
                 .GET(RUN_STATUS,       receiverLifeCycleService::runStatus)
+                .GET(NEXT_ID,          idAllocatorService::nextId)
+                .GET(NEXT_BATCH_IDS,   idAllocatorService::nextBatchIds)
                 .POST(START_ALLOCATOR, receiverLifeCycleService::start)
                 .POST(STOP_ALLOCATOR,  receiverLifeCycleService::stop)
                 .filter(MonitoringFilter::doFilter)
